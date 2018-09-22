940796cc54860dafd78413f822142fe0e7b70121
talesofpixelia <pascalperez11@yahoo.fr>
2018-09-22 17:34:28 +0200

    [PPE] Sphere Generator Completed
    Code commented
    Demo scene created

--- UKB/Assets/Sphere Generator/Scripts/Sphere.cs
+++ UKB/Assets/Sphere Generator/Scripts/Sphere.cs
@@ -7,61 +7,79 @@ public class Sphere : MonoBehaviour
 	#region Properties
 
 	/// <summary>
-    /// Gets or sets the resolution of the sphere.
+    /// Gets or sets the resolution of the sphere
     /// </summary>
     [Range(2, 256)]
 	public int resolution = 10;
 
+	/// <summary>
+    /// Gets or sets the mesh filter of the sphere
+    /// </summary>
 	[SerializeField, HideInInspector]
 	MeshFilter[] MeshFilters;
 
+	/// <summary>
+    /// Gets or sets the sphere faces of the sphere
+    /// </summary>
 	SphereFace[] SphereFaces;
 
 	#endregion Properties
 
 	#region Functions
 
+	/// <summary>
+    /// Standard method when object is on validation
+    /// </summary>
 	private void OnValidate()
 	{
 		Initialize();
 		GenerateMesh();
 	}
 
+	/// <summary>
+    /// Initialize the sphere faces on the sphere
+    /// </summary>
 	private void Initialize()
 	{
+		// Initialize the meshes if needed
 		if (MeshFilters == null || MeshFilters.Length == 0)
 		{
 			MeshFilters = new MeshFilter[6];
 		}
 
+		// Initialize the sphere faces
 		SphereFaces = new SphereFace[6];
 
+		// Defines all directions to turn un cube into
 		Vector3[] directions = { Vector3.up, Vector3.down, Vector3.left, Vector3.right, Vector3.forward, Vector3.back };
 
 		for (int i = 0; i < 6; i++)
 		{
 			if (MeshFilters[i] == null)
 			{
+				// Creation of a mesh
 				GameObject meshObj = new GameObject("mesh");
 				meshObj.transform.parent = transform;
 
+				// Default material
 				meshObj.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Standard"));
 				MeshFilters[i] = meshObj.AddComponent<MeshFilter>();
 				MeshFilters[i].sharedMesh = new Mesh();
-
-				SphereFaces[i] = new SphereFace(MeshFilters[i].sharedMesh, resolution, directions[i]);
 			}
+
+			// Creation of a sphere faces
+			SphereFaces[i] = new SphereFace(MeshFilters[i].sharedMesh, resolution, directions[i]);
 		}
 	}
 
+	/// <summary>
+    /// Generate the meshes
+    /// </summary>
 	private void GenerateMesh()
 	{
-		for (int i = 0; i < 6; i++)
+		foreach (SphereFace face in SphereFaces)
 		{
-			if (MeshFilters[i].gameObject.activeSelf)
-			{
-				SphereFaces[i].ConstructMesh();
-			}
+			face.ConstructMesh();
 		}
 	}
 	#endregion Functions
--- UKB/Assets/Sphere Generator/Scripts/SphereFace.cs
+++ UKB/Assets/Sphere Generator/Scripts/SphereFace.cs
@@ -4,59 +4,93 @@ using UnityEngine;
 
 public class SphereFace 
 {
-	Mesh mesh;
-	int resolution;
-	Vector3 localUp;
+	#region Properties
 
-	Vector3 axisA;
-	Vector3 axisB;
+	///<summary>
+	/// Gets or sets the mesh of the sphere face
+	///</summary>
+	public Mesh Mesh;
 
+	///<summary>
+	/// Gets or sets the resolution of the sphere face
+	///</summary>
+	public int Resolution;
+
+	///<summary>
+	/// Gets or sets the Vector3 of the up of the mesh face
+	///</summary>
+	public Vector3 LocalUp;
+
+	///<summary>
+	/// Gets or sets the Vector3 of the A axis relative to the up of the sphere face
+	///</summary>
+	public Vector3 AxisA;
+
+	///<summary>
+	/// Gets or sets the Vector3 of the B axis relative to the up of the sphere face
+	///</summary>
+	public Vector3 AxisB;
+
+	#endregion Properties
+
+	#region Methods
+
+	///<summary>
+	/// Initializes a new instance of the <see cref="SphereFace"/> class.
+	///</summary>
 	public SphereFace(Mesh mesh, int resolution, Vector3 localUp)
 	{
-		this.mesh = mesh;
-		this.resolution = resolution;
-		this.localUp = localUp;
+		this.Mesh = mesh;
+		this.Resolution = resolution;
+		this.LocalUp = localUp;
 
-		axisA = new Vector3(localUp.y, localUp.z, localUp.x);
-		axisB = Vector3.Cross(localUp, axisA);
+		AxisA = new Vector3(localUp.y, localUp.z, localUp.x);
+		AxisB = Vector3.Cross(localUp, AxisA);
 	}
 
+	///<summary>
+	/// Construct the Mesh
+	///</summary>
 	public void ConstructMesh()
 	{
-		Vector3[] vertices = new Vector3[resolution * resolution];
-		int[] triangles = new int[(resolution-1) * (resolution-1) * 6];
+		Vector3[] vertices = new Vector3[Resolution * Resolution];
+		
+		int[] triangles = new int[(Resolution-1) * (Resolution-1) * 6];
 
 		int triIndex = 0;
 
-		for (int y = 0; y < resolution; y++)
+		for (int y = 0; y < Resolution; y++)
 		{
-			for(int x = 0; x < resolution; x++)
+			for(int x = 0; x < Resolution; x++)
 			{
-				int i = x + y * resolution;
-				Vector2 percent = new Vector2(x,y) / (resolution-1);
-				Vector3 pointOnUnitCube = localUp + (percent.x-.5f) * 2 * axisA + (percent.y - 0.5f) * 2 * axisB;
+				int i = x + y * Resolution;
+				Vector2 percent = new Vector2(x,y) / (Resolution-1);
+				Vector3 pointOnUnitCube = LocalUp + (percent.x-.5f) * 2 * AxisA + (percent.y - 0.5f) * 2 * AxisB;
 				Vector3 pointOnUnitSphere = pointOnUnitCube.normalized;
 
 				vertices[i] = pointOnUnitSphere;
 
-				if(x != resolution - 1 && y != resolution - 1)
+				if(x != Resolution - 1 && y != Resolution - 1)
 				{
 					triangles[triIndex] = i;
-					triangles[triIndex + 1] = i + resolution + 1;
-					triangles[triIndex + 2] = i + resolution;
+					triangles[triIndex + 1] = i + Resolution + 1;
+					triangles[triIndex + 2] = i + Resolution;
 
 					triangles[triIndex + 3] = i;
 					triangles[triIndex + 4] = i + 1;
-					triangles[triIndex + 5] = i + resolution + 1;
+					triangles[triIndex + 5] = i + Resolution + 1;
 
 					triIndex += 6;
 				}
 			}
 		}
 
-		mesh.Clear();
-		mesh.vertices = vertices;
-		mesh.triangles = triangles;
-		mesh.RecalculateNormals();
+		// clear and recalc with new values
+		Mesh.Clear();
+		Mesh.vertices = vertices;
+		Mesh.triangles = triangles;
+		Mesh.RecalculateNormals();
 	}
+
+	#endregion Methods
 }
